4c912066ce0fc319e6655f099c375969cd51a0dc
Christopher Fujino <christopherfujino@gmail.com>
2026-01-20 15:00:41 -0800

    make entrypoint a test runner

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,10 +1,17 @@
 open Micro_build
 
+let eval_line line =
+  let lexbuf = Lexing.from_string line in
+  let ast = Parser.expr Scanner.read lexbuf in
+  ignore ast
+
+(*
 let rec eval () =
   let s = read_line () in
-  let lexbuf = Lexing.from_string s in
-  let ast = Parser.expr Scanner.read lexbuf in
-  ignore ast;
+  eval_line s;
   eval ()
+*)
 
-let () = eval ()
+let () =
+  let test_specs = ["42"; "21"] in
+  List.iter eval_line test_specs
